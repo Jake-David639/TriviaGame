1fdb6f7dba2896a027dd98a73bed9394c2ab9e83
Jake David <jakedgmu13@gmail.com>
2019-04-01 16:56:31 -0400

    HW5.3 wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,103 +7,123 @@ function Question(q, a, b, c, d, answer) {
     answer = this.answer;
 }
 
-const questionBank = [q1 = new Question("What is my favorite color?", "red", "yellow", "green", "blue", c),
-                q2 = new Question("Another question:", "1", "2", "3", "4", a), 
-                q3 = new Question("what do you seek?", "the holy grail", "cake", "ale", "cookies", a)];
+let questionBank = [q1 = new Question("What is the meaning of Life?", "There is no meaning to life", "To be happy", "To break the wheel of karma", "42", d),
+q2 = new Question("What piece of sports equipment is actully a truncated icosahedron?", "Uh, I flunced geometry in highschool...", "A hockey puck", "A soccer ball", "None of the above", c),
+q3 = new Question("How many Plutonic solids are there?", "Seven", "Five", "Twenty", "What is this pseudoscience nonsense!?", b),
+q4 = new Question("What element is allegedly used in the antigravity propulsion systems studied by Robert Lazar at the Los Alamos National Labs in New Mexico?", "Plutonium", "Osmium", "Di-Lithium Crystal", " Element 115", d),
+q5 = new Question("What element, other than carbon, do scientists speculate life might evolve around?", "Silicon", "Nitrogen", "Oxygen", "Busmuth", a),
+g6 = new Question("What is the speed of light in the vacuum?", "About 300,000 Km per hour", "Light does not have a speed, it has a rate of induction through a medium.", "9.8 meters per second per second", "Einsteins lame...", b),
+q7 = new Question("Whats the name of the largest machine ever made by humans?", "The Great Pyramid of Giza", "The Great Wall of China", "The Large Haydron Collider", "", c),
+q8 = new Question("Another question:", "1", "2", "3", "4", a),
+q9 = new Question("Another question:", "1", "2", "3", "4", a),
+q10 = new Question("Another question:", "1", "2", "3", "4", a),
+q11 = new Question("Another question:", "1", "2", "3", "4", a),
+q12 = new Question("Another question:", "1", "2", "3", "4", a),
+q13 = new Question("Another question:", "1", "2", "3", "4", a),
+q14 = new Question("Another question:", "1", "2", "3", "4", a),
+q15 = new Question("Another question:", "1", "2", "3", "4", a)],
 
 var totesTrivial = {
     gameRunning: false,
-    isThinking: false,
-    intervalId: "",
-    count: 15,
+    clockRunning: false,
+    intervalId: '',
+    timeLeft: 25,
     numRight: 0,
     numWrong: 0,
-    selectedAnswer: "",
-    currentQuestion: "",
+    selectedAnswer: '',
+    currentQuestion: {},
+    
+    timer: () => {
 
-    gameStart: function () {
-
-        this.gameOn = true;
-        this.questionLoop();
+        this.timeLeft--;
+        $("#clock").text(`${this.timeLeft} sec.`);
 
     },
-    // cycle through each question 
 
+    stopTimer: () => {
+
+        clearInterval(this.intervalId);
+        this.clockRunning = false;
+        
+    },
+    
+    gameInit: () => {
+
+        this.gameRunning = false;
+        this.timer = 25;
+        this.numRight = 0;
+        this.numWrong = 0;
+
+    },
+
+    startGame: () => {
+        
+        this.gameRunning = true;
+        this.questionLoop();
+        
+    },
+
+
+    // cycle through each question 
     questionLoop: () => {
 
         // Display question timer
-        for (let i = 0; i <= triviaQs.length; i++) {
-            this.isThinking = true;
-            this.intervalId = setInterval(this.countdown(), 1000);
-            this.currentQuestion = triviaQs[i];
+        for (let i = 0; i < questionBank.length; i++) {
+            this.clockRunning = true;
+            this.intervalId = setInterval(this.timer(), 1000);
+            this.currentQuestion = questionBank[i];
             console.log(this.currentQuestion);
 
 
             $("#question").text(this.currentQuestion.q);
-            $("#aText").text(this.currentQuestion.a);
-            $("#bText").text(this.currentQuestion.b);
-            $("#cText").text(this.currentQuestion.c);
-            $("#dText").text(this.currentQuestion.d);
-            $("#rightAnswer").html(`<img ${this.currentQuestion.rightA} />`);
-            $("#wrongAnswer").html(`<img ${this.currentQuestion.wrongA} />`);
+            $("#a").text(this.currentQuestion.a);
+            $("#b").text(this.currentQuestion.b);
+            $("#c").text(this.currentQuestion.c);
+            $("#d").text(this.currentQuestion.d);
 
         }
 
-        // if (this.count === 0) {
-        //     this.isThinking = false;
-        //     this.checkAnswer();
-        // }
-    },
+        if (this.timeLeft === 0) {
+            this.stopTimer();
+            this.checkAnswer();
+        }
 
-    timer: () => {
-        this.count--;
-        $("#clock").text(`00:${this.count}`);
     },
 
 
     checkAnswer: () => {
-        clearInterval(this.intervalId);
-        //if answer not subbmitter before countdown reaches 0 display time up Alert. Advance to next page.
-        this.isThinking = false;
+        //if answer not subbmitted before countdown reaches 0 display time up Alert. Advance to next page.
+
+        this.stopTimer();
         if ((this.selectedAnswer === null) && (this.count === 0)) {
-            $("#timeUp").show;
-            this.count = 15;
+            $("#gameMsg").text('Times Up!');
+            this.timer = 25;
             this.questionLoop();
-            // in answer is 
+
         } else if (currentQuestion.answer === selectedAnswer) {
-            $("#rightAnswer").show;
-        } else if (currentQuestion.answer != selectedAnswer) {
-            $("wrongAnswer").show;
+            this.numRight++;
+            $('#num-right').text(this.numRight);
+        } else if (currentQuestion.answer !== selectedAnswer) {
+            this.numWrong++;
+            $('#num-wrong').text(this.numWrong);
         }
     },
 
 
     // after all questions have cycled, show endgame stats page and render button to start a new game
-    gameInit: () => {
-        this.gameOn = false;
-        this.count = 20;
-        this.numRight = 0;
-        this.numWrong = 0;
-
-    },
 }
 
-// for (let key in totesTrivial) {
-//     if (typeof totesTrivial[key] == 'function') {
-//         totesTrivial[key] = totesTrivial[key].bind(totesTrivial);
-//     }
-// }
 
 $(document).ready(function () {
 
-    setTimeout(totesTrivial.gameStart, 5000);
+    setTimeout(totesTrivial.startGame, 5000);
 
 
-    $(".radioBtn").on("click", function () {
+    $(".answerBtn").on("click", function () {
         totesTrivial.selectedAnswer = $(this).attr("id");
     })
 
-    $("#submitA").on("click", function (event) {
+    $("#submit").on("click", function (event) {
         event.preventDefault();
         totesTrivial.checkAnswer();
     })
